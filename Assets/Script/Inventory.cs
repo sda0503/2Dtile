6af36cc083de16c4f14f51ec8d448712ad4d3953
sda0503 <sda0503@naver.com>
2024-02-01 23:17:04 +0900

    [Modify] 필요 없는 코드 수정

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -18,18 +18,10 @@ public class Inventory : MonoBehaviour
         MakeInventoty();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void MakeInventoty() 
     {
-        Debug.Log("½ÇÇà");
         for (int i = 0; i < rabbit.maxInventory; i++)
         {
-         
             float x = (i % 4) * 120f - 180f + 1570f;
             float y = (i / 4) * -120f + 200f + 540f;
             Instantiate(back, new Vector3(x, y,0), Quaternion.identity, transform);

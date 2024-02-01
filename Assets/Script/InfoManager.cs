9c00aa5cf1147695192c5f3bee63bd6c6654800f
sda0503 <sda0503@naver.com>
2024-02-01 22:05:37 +0900

    [Add] 기능 추가

--- Assets/Script/InfoManager.cs
+++ Assets/Script/InfoManager.cs
@@ -11,31 +11,47 @@ public class InfoManager : MonoBehaviour
     public GameObject player;
     Rabbit rabbit;
 
+    public GameObject expImage;
+
+    Image exp;
+
     void Awake()
     {
         rabbit = player.GetComponent<Rabbit>();
+        exp = expImage.GetComponent<Image>();
     }
 
 
     void Start()
     {
-        //초기화
-        textList[0].text = rabbit.datas[0] ;
+        CheckStatus();
+    }
+
+    public string GetCommaText(int data) 
+    { 
+        return string.Format("{0:#,###}", data); 
+    }
+
+    public void Work()
+    {
+        Debug.Log("실행");
+        rabbit.money += rabbit.lv * 1000;
+        rabbit.exp += 2;
+        rabbit.CheckPlayerStatus();
+        exp.fillAmount = (float)rabbit.exp / (float)rabbit.maxExp;
+        CheckStatus();
+    }
+
+    void CheckStatus()
+    {
+        textList[0].text = rabbit.datas[0];
         textList[1].text = rabbit.datas[1];
         textList[2].text = rabbit.datas[2];
         textList[3].text = rabbit.lv.ToString();
         textList[4].text = rabbit.exp.ToString();
         textList[5].text = rabbit.maxExp.ToString();
         textList[6].text = GetCommaText(rabbit.money);
-    }
-
-    void Update()
-    {
-
-    }
-
-    public string GetCommaText(int data) 
-    { 
-        return string.Format("{0:#,###}", data); 
+        textList[7].text = rabbit.inventory.Count.ToString();
+        textList[8].text = rabbit.maxInventoty.ToString();
     }
 }
--- Assets/Script/Rabbit.cs
+++ Assets/Script/Rabbit.cs
@@ -10,6 +10,10 @@ public class Rabbit : MonoBehaviour
     public int maxExp = 0;
     public int money = 0;
 
+    public List<int> inventory = new List<int>();
+    public int maxInventoty = 10;
+
+    Vector3 mousePos;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,32 @@ public class Rabbit : MonoBehaviour
         lv = 1;
         exp = 0;
         maxExp = 10;
-        money = 1000;
+        money = 100034587;
+    }
+
+     void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+        if(mousePos.x < 0)
+        {
+            transform.position = new Vector3(1.56f, 2.67f, 1);
+            transform.localScale = new Vector3(-4,4,1);
+        }
+        else if(mousePos.x > 0)
+        {
+            transform.position = new Vector3(-1.31f, 2.67f, 1);
+            transform.localScale = new Vector3(4, 4, 1);
+        }
+    }
+
+    public void CheckPlayerStatus()
+    {
+        if(exp >= maxExp)
+        {
+            lv++;
+            exp -= maxExp;
+            maxExp = lv * 10;
+        }
     }
 }
